5cbdca89e81b3d7a1b29cb388c6dbb085c3017e1
Kazunori Nakajima <kazunori_nakajima_aa@mail.toyota.co.jp>
2024-01-14 14:48:31 +0900

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/pokemon-silhouette-quiz/sw.js', { scope: '/pokemon-silhouette-quiz/' })})}
